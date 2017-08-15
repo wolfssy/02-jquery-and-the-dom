b69272676a14d93f857e24396208cab51f80ef1e
Brandon Arroyo <brandon.jarroyo@outlook.com>
2017-08-15 14:13:15 -0700

    did 3/4 with article.js54 out

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -1,18 +1,27 @@
 'use strict';
 
+
+
 var articles = [];
 
 function Article (rawDataObj) {
-  // TODO: Use the JS object passed in to complete this constructor function:
+  // TODO: (done) Use the JS object passed in to complete this constructor function:
   // Save ALL the properties of `rawDataObj` into `this`
+  this.title = rawDataObj.title;
+  this.category = rawDataObj.category;
+  this.author = rawDataObj.author;
+  this.authorUrl = rawDataObj.authorUrl;
+  this.publishedOn = rawDataObj.publishedOn;
+  this.body = rawDataObj.body;
 }
 
 Article.prototype.toHtml = function() {
   var $newArticle = $('article.template').clone();
-  /* TODO: This cloned article still has a class of template.
+  /* TODO: (Done)This cloned article still has a class of template.
   However, in our modules.css stylesheet, we gave all elements
   with a class of template a display of none. Let's make
   sure we're not accidentally hiding our cloned article! */
+  $newArticle.removeClass('template'); // NEW.
 
   if (!this.publishedOn) $newArticle.addClass('draft');
   $newArticle.data('category', this.category);
@@ -42,6 +51,6 @@ rawData.forEach(function(articleObject) {
   articles.push(new Article(articleObject));
 });
 
-articles.forEach(function(article) {
-  $('#articles').append(article.toHtml());
-});
+// articles.forEach(function(article) {
+//   $('#articles').append(article.toHtml());
+// });
